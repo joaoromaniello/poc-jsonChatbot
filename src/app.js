8dfb491280348d66c187d465f0980ee1c88c403f
Joao Nardini <joao.nardini@b2list.com>
2024-06-24 17:31:12 -0300

    more logics...

--- src/app.js
+++ src/app.js
@@ -1,53 +1,56 @@
-const QRCODE = require('qrcode-terminal');
-const { Client } = require('whatsapp-web.js');
-const chatFlow = require('../Fluxes/jsonTest.json');
+const QRCODE = require("qrcode-terminal");
+const { Client } = require("whatsapp-web.js");
+const chatFlow = require("../Fluxes/jsonTest.json");
 
 const client = new Client();
-let currentStateId = null;  
-
+let currentStateId = null;
 
 async function sendMessage(message, chatId) {
-    await client.sendMessage(chatId, message);
+  await client.sendMessage(chatId, message);
 }
 
 async function showState(stateId, chatId) {
-    currentStateId = stateId;  
-    const state = chatFlow.states[stateId];
-    let message = state.message + "\n";
+  currentStateId = stateId;
+  const state = chatFlow.states[stateId];
+  let message = state.message + "\n";
+
+  if (chatFlow.states[stateId].type == "baseMessage") {
     state.options.forEach((option, index) => {
-        message += `\n*${index + 1}*. ${option.text}`;
+      message += `\n*${index + 1}*. ${option.text}`;
     });
+
     await sendMessage(message, chatId);
+    return;
+  } else if (chatFlow.states[stateId].type == "baseMessage") {
+    return;
+  }
 }
 
-
-client.on('qr', (qr) => {
-    QRCODE.generate(qr, { small: true });
+client.on("qr", (qr) => {
+  QRCODE.generate(qr, { small: true });
 });
 
-client.on('ready', () => {
-    console.log('Client is ready!');
+client.on("ready", () => {
+  console.log("Client is ready!");
 });
 
+client.on("message", async (msg) => {
+  if (!currentStateId || currentStateId === "end") {
+    currentStateId = chatFlow.initialState;
+    await showState(currentStateId, msg.from);
+    return;
+  }
 
-client.on('message', async msg => {
-    if (!currentStateId || currentStateId === "end") {
-
-        currentStateId = chatFlow.initialState;
-        await showState(currentStateId, msg.from);
-        return;
-    }
-
-    const currentState = chatFlow.states[currentStateId];
-    const message = msg.body.trim();
-    const choiceIndex = parseInt(message, 10) - 1;
-    if (choiceIndex >= 0 && choiceIndex < currentState.options.length) {
-        const nextStateId = currentState.options[choiceIndex].next;
-        await showState(nextStateId, msg.from);
-    } else {
-        await sendMessage('Opção inválida. Tente novamente.', msg.from);
-        await showState(currentStateId, msg.from); 
-    }
+  const currentState = chatFlow.states[currentStateId];
+  const message = msg.body.trim();
+  const choiceIndex = parseInt(message, 10) - 1;
+  if (choiceIndex >= 0 && choiceIndex < currentState.options.length) {
+    const nextStateId = currentState.options[choiceIndex].next;
+    await showState(nextStateId, msg.from);
+  } else {
+    await sendMessage("Opção inválida. Tente novamente.", msg.from);
+    await showState(currentStateId, msg.from);
+  }
 });
 
 client.initialize();
